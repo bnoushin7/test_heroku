315f16eb9b75d44f6a4d4b2617607d8fe6a5df67
bnoushin7 <noushin7@vt.edu>
2020-08-13 22:03:45 -0400

    about

--- app.py
+++ app.py
@@ -4,3 +4,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, Noushin Jooon, problem solved!'
+
+
+@app.route('/about')
+def about():
+    return 'This is about Noushin'
