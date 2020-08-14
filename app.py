e1f8a02c544035a6d14081b2c2cf8348a2131e69
bnoushin7 <noushin7@vt.edu>
2020-08-14 17:16:11 -0400

    sdjh

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import json
 from flask import request, jsonify, make_response
 app = Flask(__name__)
 
@@ -13,14 +14,13 @@ def about():
 
 
 
-
+"""
 @app.route('/json', methods=["POST"])
 def json():
 
     if request.is_json:
 
         req = request.get_json()
-
         response_body = {
             "message": "JSON received!",
             "sender": req.get("message")
@@ -31,4 +31,25 @@ def json():
 
     else:
         print("Not Done!")
-        return '"message": "Request body must be JSON"}), 400'        
+        return '"message": "Request body must be JSON"}), 400'     
+
+"""
+@app.route('/json', methods=["POST"])
+def json():
+
+    if not request.json:
+        return make_response(jsonify({
+            "code": 400,
+            "message": "Input should be a json"
+        }), 400)
+    try:
+        body = request.json
+    except:
+        return make_response(jsonify({
+            "code": 400,
+            "message": "Failed to parse input json"
+        }), 400)
+    return make_response(jsonify({
+            "code": 200,
+            "message": "success"
+        }), 200)
