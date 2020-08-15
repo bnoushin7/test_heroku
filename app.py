cef3b583be26962c7f3d17891e0b860f0b016dc7
bnoushin7 <noushin7@vt.edu>
2020-08-15 14:53:14 -0400

    sdfs

--- app.py
+++ app.py
@@ -2,25 +2,10 @@ from flask import Flask
 import json
 from flask import request, jsonify, make_response
 from intake import open_catalog
+
+import pandas
+import re
 app = Flask(__name__)
-
-def my_recursive(catalog):
-    for i in list(catalog):
-        queue.append(i)
-   
-        while queue:
-            level_size = len(queue)
-            for _ in range(level_size):
-                current = queue.pop()
-                result.append(current)
-                subcat=open_catalog(catalog_name+current.lower()+'.yaml')
-
-
-                for j in list(subcat):
-                    if j not in ('netcdf','example'): 
-                        queue.append(j)
-    return result
-
 
 @app.route('/')
 def hello_world():
@@ -67,11 +52,45 @@ def json():
         }), 400)
     try:
         body = request.json
+        tagg = body['tag']
+        print(tagg)
     except:
         return make_response(jsonify({
             "code": 400,
             "message": "Failed to parse input json"
         }), 400)
+    return make_response(jsonify({
+            "code": 200,
+            "sender": body['tag'],
+            "message": "success"
+        }), 200)
+
+
+
+@app.route('/data_per_tag', methods=["POST", "GET"])
+def datasets_per_tag(tag):
+    # df = pandas.read_excel('tags-datasets.xlsx')
+    # my_list = df[lambda x: ~pandas.isnull(x[tag])][tag]
+    # pattern = re.compile(r'\s+')
+    # s = my_list.to_string(index=False).replace('\n',',')
+    # return(print(pattern.sub(' ', s)))
+
+    if request.method == 'POST':
+        if not request.json:
+            return make_response(jsonify({
+                "code": 400,
+                "message": "Input should be a json"
+            }), 400)
+        try:
+            body = request.json
+            tag = body['tag']
+            print(tag)
+        except:
+            return make_response(jsonify({
+                "code": 400,
+                "message": "Failed to parse input json"
+            }), 400)
+
     return make_response(jsonify({
             "code": 200,
             "sender": body,
