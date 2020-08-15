c195f6fad2d6433c1160989e504301fd59ad90d2
bnoushin7 <noushin7@vt.edu>
2020-08-14 20:08:17 -0400

    flask working

--- app.py
+++ app.py
@@ -14,26 +14,6 @@ def about():
 
 
 
-"""
-@app.route('/json', methods=["POST"])
-def json():
-
-    if request.is_json:
-
-        req = request.get_json()
-        response_body = {
-            "message": "JSON received!",
-            "sender": req.get("message")
-        }
-
-        res = make_response(jsonify(response_body), 200)
-        return res
-
-    else:
-        print("Not Done!")
-        return '"message": "Request body must be JSON"}), 400'     
-
-"""
 @app.route('/json', methods=["POST"])
 def json():
 
