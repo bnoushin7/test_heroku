5318faa14092e79aade01e6117ff44afcf3b0197
bnoushin7 <noushin7@vt.edu>
2020-08-16 16:58:31 -0400

    aeemed to be working

--- app.py
+++ app.py
@@ -1,98 +1,70 @@
-from flask import Flask
-import json
-from flask import request, jsonify, make_response
-from intake import open_catalog
-
+from flask import Flask, request, jsonify, make_response
 import pandas
+from intake import open_catalog
 import re
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Noushin Jooon, problem solved!'
+    return 'Hello, World!'
 
-
-@app.route('/about')
-def about():
-    return 'This is about Noushin'
-
-
-@app.route('/all_data')
-def all_data():
-
-    catalog_name='https://raw.githubusercontent.com/kpegion/COLA-DATASETS-CATALOG/gh-pages/intake-catalogs/'
-    cat = open_catalog(catalog_name+'master.yaml')
-    result = []
-    queue = []
-    def my_recursive(catalog):
-        for i in list(catalog):
-            queue.append(i)
-
-            while queue:
-                level_size = len(queue)
-                for _ in range(level_size):
-                    current = queue.pop()
-                    result.append(current)
-                    subcat=open_catalog(catalog_name+current.lower()+'.yaml')
-
-
-                    for j in list(subcat):
-                        if j not in ('netcdf','example'):
-                            queue.append(j)
-        return result
-    return my_recursive(cat)
-
-@app.route('/json', methods=["POST"])
-def json():
-
-    if not request.json:
-        return make_response(jsonify({
+@app.route("/tag_based", methods=["POST"])
+def datasets_per_tag():
+	res = ""
+	if not request.json:
+		return make_response(jsonify({
             "code": 400,
             "message": "Input should be a json"
-        }), 400)
-    try:
-        body = request.json
-        tagg = body['tag']
-        print(tagg)
-    except:
-        return make_response(jsonify({
+		}), 400)
+	try:
+		body = request.json
+		tag = body['tag']
+		df = pandas.read_excel('tags-datasets.xlsx')
+		my_list = df[lambda x: ~pandas.isnull(x[tag])][tag]
+		print("33")
+		pattern = re.compile(r'\s+')
+		s = my_list.to_string(index=False).replace('\n',',')
+		#return(print(pattern.sub(' ', s)))
+		res = pattern.sub(' ', s)
+	except Exception as inst:
+		print(res)
+		return make_response(jsonify({
             "code": 400,
             "message": "Failed to parse input json"
-        }), 400)
-    return make_response(jsonify({
+		}), 400)
+	return make_response(jsonify({
             "code": 200,
-            "sender": body['tag'],
+            "result": res,
             "message": "success"
-        }), 200)
+		}), 200)
+
+		
+@app.route('/all_data')
+def all_data():
+	catalog_name='https://raw.githubusercontent.com/kpegion/COLA-DATASETS-CATALOG/gh-pages/intake-catalogs/'
+	cat = open_catalog(catalog_name+'master.yaml')
+	result = []
+	queue = []
+	new_name = ""
+	def my_recursive(catalog):
+		for i in list(catalog):
+			queue.append(i)
+
+			while queue:
+				level_size = len(queue)
+				for _ in range(level_size):
+					current = queue.pop()
+					result.append(current)
+					new_name = catalog_name+current.lower()+'.yaml'
+					subcat=open_catalog(new_name)
+					for j in list(subcat):
+						if j not in ('netcdf','example'):
+							queue.append(j)
+		return jsonify(result)
+		print("66666666666666666666666")
+	return(my_recursive(cat))
 
 
-
-@app.route('/data_per_tag', methods=["POST", "GET"])
-def datasets_per_tag(tag):
-    # df = pandas.read_excel('tags-datasets.xlsx')
-    # my_list = df[lambda x: ~pandas.isnull(x[tag])][tag]
-    # pattern = re.compile(r'\s+')
-    # s = my_list.to_string(index=False).replace('\n',',')
-    # return(print(pattern.sub(' ', s)))
-
-    if request.method == 'POST':
-        if not request.json:
-            return make_response(jsonify({
-                "code": 400,
-                "message": "Input should be a json"
-            }), 400)
-        try:
-            body = request.json
-            tag = body['tag']
-            print(tag)
-        except:
-            return make_response(jsonify({
-                "code": 400,
-                "message": "Failed to parse input json"
-            }), 400)
-
-    return make_response(jsonify({
-            "code": 200,
-            "sender": body,
-            "message": "success"
-        }), 200)
+if __name__ == "__main__":
+    app.config['JSON_AS_ASCII'] = False
+    app.run()
